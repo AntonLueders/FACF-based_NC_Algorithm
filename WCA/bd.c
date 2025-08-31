e4edbb153b5eed9dbdda5db8b453242eaea09fc1
AntonLueders <145451222+AntonLueders@users.noreply.github.com>
2025-08-31 02:01:54 +0200

    Update bd.c

--- WCA/bd.c
+++ WCA/bd.c
@@ -1,6 +1,8 @@
 #include "bd.h"
 #include "distance.h"
 
+// ----------------------------------------------------------------------------------------
+
 void BrownianMotion(Particle *P) {
 
 	for(int i = 0; i < N; i++) {
@@ -8,8 +10,10 @@ void BrownianMotion(Particle *P) {
 		for(int d = 0; d < dim; d++) {
 			double Noise = sqrt(2. * P[i].D * dt) * gsl_ran_gaussian(generator, 1.0);
 			P[i].r[d] += P[i].D * P[i].f[d] * dt + Noise;
-			P[i].r[d] = PeriodicPos(P[i].r[d], L);
+			P[i].r[d] = PeriodicPos(P[i].r[d], L); // PBCs. See distance.c
 			P[i].Noise[d] = Noise;
 		}
 	}
 }
+
+// ----------------------------------------------------------------------------------------
